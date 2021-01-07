bff3b274e45a739277933bd477d2c1d759b63c5e
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2021-01-07 08:15:57 -0300

    add tests

--- test/src/Sim.js
+++ test/src/Sim.js
@@ -3,14 +3,48 @@ const sinon = require('sinon')
 const EventEmitter = require('events')
 
 const Sim = require('../../src/Sim')
+const AtCommand = require('../../src/at/commands/ping')
 const AtEvent = require('../../src/at/events/received-sms')
 
 describe('Sim', () => {
+  afterEach(() => {
+    sinon.restore()
+  })
+
+  describe('#ping', () => {
+    it('executes the command', () => {
+      const fakeCommand = 'command'
+      const fakeResponse = 'response'
+      const fakeDevice = new EventEmitter()
+
+      const stubCommand = sinon.stub(AtCommand, 'command').returns(fakeCommand)
+      const stubHandler = sinon.stub(AtCommand, 'handle')
+      fakeDevice.send = sinon.stub().resolves(fakeResponse)
+
+      const sim = new Sim(fakeDevice)
+
+      return sim.ping()
+        .then(() => {
+          expect(stubCommand.called).to.be.true;
+          expect(fakeDevice.send.called).to.be.true;
+          expect(stubHandler.calledWithExactly(fakeResponse)).to.be.true;
+        })
+        .catch(() => {
+          assert.fail()
+        })
+    })
+  })
 
   describe('#_onEvent', () => {
-    const fakeDevice = new EventEmitter()
-    const sim = new Sim(fakeDevice)
+    it('found applicable event', () => {
+      const stubHandler = sinon.stub(AtEvent, 'handle')
 
-    fakeDevice.emit('event', ['+CMTI: "SM",6'])
+      const payload = ['+CMTI: "SM",6']
+      const fakeDevice = new EventEmitter()
+      const sim = new Sim(fakeDevice)
+
+      fakeDevice.emit('event', payload)
+      expect(stubHandler.calledWithExactly(['6'], [])).to.be.true
+    })
   })
 })
